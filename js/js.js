01b97f0f7a0a2821c37d351cb54d7d504cb8f4f9
Ivan Fuentes <ivantxp@gmail.com>
2022-12-09 11:06:19 -0300

    9/12

--- js/js.js
+++ js/js.js
@@ -1,3 +1,20 @@
 
-let datos_nombre = prompt("bienvenido a Fast Ice Cream, como te llamas")
-alert ("bienvenido " + datos_nombre)
+
+
+function nombre(){
+    let datos_nombre = prompt("hola como te llamas")
+    if( datos_nombre == ""){
+        while(datos_nombre === ' '){
+            datos_nombre = window.prompt("debe ingresar un nombre")
+        }
+    }else{
+        while(datos_nombre.length > 10){
+            datos_nombre = prompt("el nombre no puede contener mas de 10 caracteres")
+        }
+    }
+    console.log(datos_nombre);
+    alert ("bienvenido a Fast Ice Cream " + datos_nombre)
+
+}
+
+nombre()
