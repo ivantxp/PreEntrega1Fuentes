be10fa3ad6c6d4cb4198ba964687327170ab2fda
ivantxp <ivantxp@gmail.com>
2023-01-09 13:06:00 -0300

    f

--- js/js.js
+++ js/js.js
@@ -179,6 +179,14 @@ function valor_si_no(pregunta){
     return si_no;
 }
 
+function control_numero(pregunta){
+    let numero = Number(prompt(pregunta));
+    while(isNaN(numero)  && numero != null){
+        si_no = prompt("Debe ingresar un nuemero de la lista");
+        console.log(numero);
+    }
+    return numero;
+}
 function nombre(){
     let ingreso_nombre = control_ingreso( "Hola como te llamas", 10, "debe ingresar un nombre que no supere los ");
     alert("Bienvenido " + ingreso_nombre + " a Fast Ice Cream" );
@@ -188,29 +196,94 @@ let formato = [
     {id: 1,tipo: "vasito de 250g",precio: 150},
     {id: 2,tipo: "casata de 500g",precio: 250},
     {id: 3,tipo: "resipiente termico de 1k",precio: 400},
+    {id: 4 ,tipo: "resipiente termico de 2k",precio: 700},
 ]
 let comprados= [];
 
+
+let categoria_selecionada
+let a =[]
+helados.forEach((el)=> a.push(el.categoria))
+
+console.log(a)
+/*
+
+2. Pedir al usuario un nombre de una película y en caso de que se encuentre en el 
+array movies mostrar por pantalla la cantidad de votos que tiene.
+
+const busqueda = prompt("ingrese nombre pelicula")
+let buscado = movies.find((el) => el.title.toLocaleLowerCase() ==  busqueda.toLocaleLowerCase())
+
+
+if(buscado == undefined){
+    alert("pelicula no existe")
+}else{
+    alert(buscado.vote_average)
+}
+*/
+
+// array compra debe tener------sabor ---preccio ---- cantidad-----sub total
 function comprar(){
     if(valor_si_no("Quieres comprar un helado?") =="si"){
-        let tipo
         do{
-            tipo = Number(prompt( "Los formatos que puede elejir son: \n" + formato.map((el)=>{return el.id +  el.tipo + "-->" + el.precio }).join("\n")+"\nSeleccione una opcion") );
-            alert(tipo)
-            alert(formato.some((el) => {el.id == tipo }))
+            
+            let sabor 
+            let tipo = 0
+            let cantidad
 
-        }while(formato.some((el) => {el.id != tipo }) )   
+            categoria_selecionada = prompt("las categorias de healdos que tenemos son: \n1) Cremas. \n2) chocolates. \n3) dulce de leches \n4) Frutas al agua. \n5) Frutas a la Cremas. \n6) Sambayones.\n Escriba el numero de la opcion que quiera" )
+            let filtro = helados.filter((el)=>el.categoria === categoria_selecionada)
+            let indece = 1
+            sabor = ( prompt("los sabores que tenemos con esa categoria son: \n"+  filtro.map((el)=>{
+                return indece++  + ")" +el.sabor}).join("\n") + "\n-dige el numero de cual quiere?")    )
+            tipo = prompt( "Los formatos que puede elejir son: \n" + formato.map((el)=>{return el.id +  el.tipo + "-->" + el.precio }).join("\n")+"\nSeleccione una opcion") ;
+            cantidad = Number(prompt("cuantos " + tipo + " quiere"))
+            
+            comprados.push(
+                {sabor: sabor, precio: tipo, cantidad: cantidad, sub_total: tipo * cantidad
+                }
+            )   
+        }while( valor_si_no("quiere continuar coprado?") == "si");
 
-        alert(tipo)
-        comprados.push({id:tipo})
-        alert(comprados);
+        console.log(comprados)
+        let total =  comprados.reduce((a,el)=> a + el.sub_total,0)
+        alert(total)
+        alert("en su carrito tiene:\n" + comprados.map((el)=>{return el.sabor + "precio " + el.precio + "x" + el.cantidad + " = " + el.sub_total}).join("\n") + "\ntotal = " + total)
+        if(valor_si_no("quiere modificar algun helemento del carrito" =="si")){
+
         }
-
-      
+    }else{
         alert("Agradesemos tu visita" );
+    }
     }
 let nombre_usuario = nombre()
 comprar()
+
+
+  /*
+function comprar(){
+    if(valor_si_no("Quieres comprar un helado?") =="si"){
+        let tipo = 0
+        let valor
+        let categorias_helado
+        do{
+            categorias_helado = control_numero( "las categorias de healdos que tenemos son: \n1) Cremas. \n2) chocolates. \n3) dulce de leches \n4) Frutas al agua. \n5) Frutas a la Cremas. \n6) Sambayones.\n Escriba el numero de la opcion que quiera"  ) ;
+        }while(categorias_helado < 0 && categorias_helado > 6 ) 
+        alert(categorias_helado)
+       do{
+            tipo = Number(prompt( "Los formatos que puede elejir son: \n" + formato.map((el)=>{return el.id +  el.tipo + "-->" + el.precio }).join("\n")+"\nSeleccione una opcion") );
+            valor = formato.some((el) => el.id == tipo )
+            alert(valor)
+
+        }while(valor == false) 
+    
+        
+        alert(comprados); 
+        }
+        alert("Agradesemos tu visita" );
+    }
+let nombre_usuario = nombre()
+comprar()*/
 
 /* 
 function compra(){
